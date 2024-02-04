18ec7195c8c14ce50651e1765e9ea88da40c8b9c
ayoub <ayouubcharef@gmail.com>
2024-02-04 19:40:27 +0100

    search journal name step

--- journal.php
+++ journal.php
@@ -8,37 +8,33 @@ error_reporting(0);
 <?php
 include 'functions.php';
 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
-
-
 if (isset($_GET['start'])) 
 {
 
 	$page=1;
-	if (isset($_GET['page'])) 
+	if (isset($_SESSION['page'])) 
 	{
-		$page=$_GET['page'];
+		$page=$_SESSION['page'];
+	}else
+	{
+		$_SESSION['page']=1;
 	}
-	exit("we are here ");
-	$link="https://www.scimagojr.com/journalrank.php?page".$page."1&total_size=27955";
+	
+	$link="https://www.scimagojr.com/journalrank.php?page=".$page."&total_size=27955";
 	$_SESSION['link']	= $link;
 
 		//reload
-	echo '<script>location.href="chapter.php";</script>';
+	echo '<script>location.href="journal.php";</script>';
 
 } elseif (isset($_GET['new_journal'])) 
 {
 
 	
-    //======= Get info journal =======//
+    //======= get All journal in each page=======//
 	$html = curl($newChap);
 	$idom = new DOMDocument();
 	@$idom->loadHTML($html);
 	$ixpath = new DOMXPath($idom);
-
-	
 		
 
     // Insert Content and Update List link
@@ -48,20 +44,13 @@ if (isset($_GET['start']))
 	}else
 	{
 	
-		echo '<script>location.href="chapter.php";</script>';
+		echo '<script>location.href="journal.php";</script>';
 		exit();
 	}
 
-	
-
-
-
-
 } elseif(isset($_SESSION['link'])) 
 {
-
-
-
+	
 	echo 'Link: '.$_SESSION['link'].'<br>';
 	$source= $_SESSION['link'];
 	$html = curl($source);
@@ -69,12 +58,22 @@ if (isset($_GET['start']))
 	@$dom->loadHTML($html);
 	$xpath = new DOMXPath($dom);
 	//--------------- get class div of all hrefs ------------
-	
-	
+	$xpath_name="//div[@class='table_wrap']//table//td[@class='tit']/a";
+	$hrefs = $xpath->evaluate($xpath_name);
+	$journals_name = array();
+
+	for ($i = 0; $i < $hrefs->length; $i++) 
+	{
+		$href = $hrefs->item($i);
+		$link = $href->getAttribute('href');
+		$journals_name[]="https://www.scimagojr.com/".$link;
+	}
+
+	print_r($journals_name);
 	
 }
 else
 {
 	echo 'Finished running!';
-	echo '<script>location.href="chapter.php?start=1";</script>';
+	echo '<script>location.href="journal.php?start=1";</script>';
 }

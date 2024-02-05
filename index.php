50e0945434484978b9f987d796b6bb1bc4f334b1
ayoub <ayouubcharef@gmail.com>
2024-02-05 05:14:07 +0100

    create scraper and journal files

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
-<a href="journal.php?start=1"><button style="    height: 200px;
-    width: 100%;">Start Scimago Scraper</button></a>
+<?php
+include 'functions.php';
+?>
+
+
+<a href="scraper.php?start=1"><button style="    height: 200px;
+    width: 100%;">Start Scimago Scraper with Page <?=getPage() ?></button></a>
